ad90d9798f89616c0aab2a5b041915479a65d91f
NonsoBarn <102404755+NonsoBarn@users.noreply.github.com>
2023-03-02 15:57:05 +0100

    env updated

--- FirebaseAuth/src/firebase.js
+++ FirebaseAuth/src/firebase.js
@@ -2,13 +2,13 @@ import { getAuth } from "@firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAsWmgTB-UjtS7EaGsm23opulWUonQgwhU",
-  authDomain: "react-authenticatin.firebaseapp.com",
-  projectId: "react-authenticatin",
-  storageBucket: "react-authenticatin.appspot.com",
-  messagingSenderId: "1002672815710",
-  appId: "1:1002672815710:web:e452e98d3a8140b0b17ecf",
-  measurementId: "G-N1YBB3YHD7",
+  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_REACT_APP_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASUREMENTID,
 };
 
 // Initialize Firebase
